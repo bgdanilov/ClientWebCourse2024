cda7662e953ec092432f3ace3e0dba6224f41e79
bgdanilov <bdan@yandex.ru>
2024-08-23 15:14:07 +0700

    Задача JS_Arrays. Исправления от 23.08.2024

--- JS_Arrays/js_arrays.js
+++ JS_Arrays/js_arrays.js
@@ -1,57 +1,53 @@
 "use strict";
 
-const array = [2, 5, 1, 0, 7, 12, 9];
-
 (function () {
+    function getDescendingSortedArray(array) {
+        return array.slice().sort((e1, e2) => e2 - e1);
+    }
+
+    function getFirstFiveNumbers(array) {
+        return array.slice(0, 5);
+    }
+
+    function getLastFiveNumbers(array) {
+        return array.slice(-5);
+    }
+
+    function getEvenNumbersSum(array) {
+        return getEvenNumbers(array).reduce((sum, item) => sum + item, 0);
+    }
+
+    function getEvenNumbersSquares(array) {
+        return getEvenNumbers(array).map(e => e * e);
+    }
+
+    function getEvenNumbers(array) {
+        return array.filter(e => (e % 2 === 0));
+    }
+
+    // 1. Создайте массив чисел.
+    const array = [2, 5, 1, 0, 7, 12, 9];
     console.log("Исходный массив: " + array);
 
-    // 1. Сортировка по убыванию.
-    let sorted = Array.from(array);
-    sorted.sort(function (a, b) {
-        return b - a;
-    });
+    // 1.1. Сортировка по убыванию.
+    console.log("Новый массив в порядке убывания: " + getDescendingSortedArray(array));
 
-    console.log("Массив в порядке убывания: " + sorted);
-    console.log("Исходный массив: " + array);
+    // 1.2. Подмассив из первых 5 элементов.
+    console.log("Подмассив из первых 5 элементов: " + getFirstFiveNumbers(array));
 
-    // 2.1. Подмассив из первых 5 элементов.
-    let first_five_numbers = Array.from(array).slice(0, 5);
-    console.log("Подмассив из первых 5 элементов: " + first_five_numbers
+    // 1.3. Подмассив из последних 5 элементов.
+    console.log("Подмассив из последних 5 элементов: " + getLastFiveNumbers(array));
 
-    );
+    // 1.4. Сумма элементов массива, которые являются четными числами.
+    console.log("Сумма элементов массива, которые являются четными числами: " + getEvenNumbersSum(array));
 
-    // 2.2. Подмассив из последних 5 элементов.
-    let last_five_numbers = Array.from(array).slice(array.length - 5);
-    console.log("Подмассив из последних 5 элементов: " + last_five_numbers);
-
-    // 3. Сумма элементов массива, которые являются четными числами.
-    let even_numbers = get_even_numbers(array);
-
-    let even_numbers_summ = 0;
-    even_numbers.forEach(function (e) {
-        even_numbers_summ += e;
-    });
-
-    console.log("Сумма элементов массива, которые являются четными числами: " + even_numbers_summ);
-})();
-
-(function () {
-    // Список квадратов четных чисел из массива чисел 1 - 100.
-    let numbers = [];
+    // 2. Создайте массив чисел от 1 до 100, в таком порядке.
+    const numbers = [];
 
     for (let i = 1; i <= 100; i++) {
         numbers.push(i);
     }
 
-    let even_numbers_squares = get_even_numbers(numbers).map(function (e) {
-        return e * e;
-    });
-
-    console.log("Список квадратов четных чисел из массива чисел 1 - 100: " + even_numbers_squares);
+    // 2.1. Получите список квадратов четных чисел из этого массива.
+    console.log("Список квадратов четных чисел из массива чисел 1 - 100: " + getEvenNumbersSquares(numbers));
 })();
-
-function get_even_numbers(array) {
-    return array.filter(function (a) {
-        return a % 2 === 0;
-    });
-};
