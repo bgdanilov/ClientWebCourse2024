c7d903243004ba76c14c914fb33ab28033f6d86d
bgdanilov <bdan@yandex.ru>
2024-09-27 09:26:17 +0700

    PhonebookVue-Input

--- PhoneBookBootstrapVue/scripts/phonebook_vue_bootstrap.js
+++ PhoneBookBootstrapVue/scripts/phonebook_vue_bootstrap.js
@@ -4,41 +4,116 @@ Vue.createApp({})
         data() {
             return {
                 contacts: [],
-                isEditing: false,
+                initialContactId: 1,
                 firstNameFieldText: "",
-                initialContactId: 1
+                lastNameFieldText: "",
+
+                isEditing: false,
+                editingContact: {},
+                editingContactId: "",
+
+                isContactFormReset: true,
+                isFirstNameFieldReset: true,
+                isLastNameFieldReset: true,
+
+                isFirstNameFieldValid: false,
+                isLastNameFieldValid: false
             };
         },
 
         methods: {
-            addContact() {
-                const newContact = {
-                    id: this.initialContactId,
-                    firstName: this.firstNameFieldText
-                };
+            onSubmit(event) {
+                const submitButtonPressed = event.submitter.name;
 
-                this.initialContactId++;
-                this.firstNameFieldText = "";
-                this.contacts.push(newContact);
+                if (submitButtonPressed === "add") {
+                    this.addContact();
+                } else if (submitButtonPressed === "save") {
+                    this.saveContact();
+                }
             },
 
-            bbb(event) {
-                this.firstNameFieldText = event;
+            onEdit(event) {
+                this.editingContactId = event[0];
+
+                this.firstNameFieldText = event[1];
+                this.lastNameFieldText = event[2];
+
                 this.isEditing = true;
-            }
+            },
+
+            onDelete(event) {
+                this.deleteContact(event);
+            },
+
+            addContact() {
+                console.log(this.isFirstNameFieldValid);
+                console.log(this.isLastNameFieldValid);
+
+                if (this.isFirstNameFieldValid && this.isLastNameFieldValid) {
+                    const newContact = {
+                        id: ++this.initialContactId,
+                        firstName: this.firstNameFieldText,
+                        lastName: this.lastNameFieldText
+                    };
+
+                    this.contacts.push(newContact);
+                    this.resetContactForm();
+                } else {
+                    this.isFirstNameFieldReset = false;
+                    this.isLastNameFieldReset = false;
+                }
+            },
+
+            saveContact() {
+                if (!this.isFirstNameFieldValid || !this.isLastNameFieldValid) {
+                    return;
+                }
+
+                this.editingContact = this.contacts.find(contact => contact.id === this.editingContactId);
+
+                this.editingContact.firstName = this.firstNameFieldText;
+                this.editingContact.lastName = this.lastNameFieldText;
+
+                this.resetContactForm();
+            },
+
+            deleteContact() {
+                const deletedContactIndex = this.contacts.findIndex(contact => contact.id === this.editingContactId);
+                this.contacts.splice(deletedContactIndex, 1);
+                this.resetContactForm();
+            },
+
+            resetContactForm() {
+                this.firstNameFieldText = "";
+                this.lastNameFieldText = "";
+
+                this.isFirstNameFieldReset = true;
+                this.isLastNameFieldReset = true;
+
+                this.isFirstNameFieldValid = false;
+                this.isLastNameFieldValid=  false;
+
+                this.isEditing = false;
+            },
         },
 
-
         template:  `
-          <form @submit.prevent="addContact" class="row">
-            <label class="col">
-              <input v-model="firstNameFieldText" class="form-control" type="text">
-            </label>
-            <div v-if = "!isEditing" class="col-auto">
-              <button class="btn btn-primary">Add</button>
-            </div>
-            <div v-else class="col-auto">
-              <button class="btn btn-primary">Save</button>
+          <form @submit.prevent="onSubmit" class="row needs-validation" novalidate>
+            <text-input :text=this.firstNameFieldText
+                        :isReset="isFirstNameFieldReset"
+                        :isValid="isFirstNameFieldValid"
+                        @update="firstNameFieldText = $event[0]; isFirstNameFieldValid = $event[1]; isFirstNameFieldReset=false"></text-input>
+            <text-input :text=this.lastNameFieldText
+                        :isReset="isLastNameFieldReset"
+                        :isValid="isLastNameFieldValid"
+                        @update="lastNameFieldText = $event[0]; isLastNameFieldValid = $event[1]; isLastNameFieldReset=false"></text-input>
+            <div class="col">
+                <div v-if="!isEditing" class="col-auto">
+                  <button name="add" class="btn btn-primary">Add</button>
+                </div>
+                <div v-else class="col-auto">
+                  <button name="save" class="btn btn-primary">Save</button>
+                </div>
             </div>
           </form>
 
@@ -46,9 +121,52 @@ Vue.createApp({})
             <phone-book-contact v-for="contact in contacts"
                                 :key="contact.id"
                                 :contact="contact"
-                                @edit-item="bbb($event)">
+                                @edit-item="onEdit($event)"
+                                @del-item="onDelete($event)">
             </phone-book-contact>
           </table>`
+    })
+
+    .component("TextInput", {
+        props: ['isReset', 'text'],
+
+        // Конструктор компонента - начальное состояние.
+        data() {
+            return {
+                isFieldValid: false,
+            };
+        },
+
+        methods: {
+            onInput(e) {
+                this.$emit('update', [e.target.value, this.isFieldValid]);
+            }
+        },
+
+        computed: {
+            calcValue() {
+                this.isFieldValid = this.text.trim().length !== 0;
+                console.log("isFieldValid = " + this.isFieldValid);
+                return this.text;
+            },
+
+            validClass() {
+                if (this.isReset) {
+                    return  "";
+                } else {
+                    return  this.isFieldValid ? "is-valid" : "is-invalid";
+                }
+            }
+        },
+
+        template: `
+          <div class="col">
+            <label for="validationCustom03" class="form-label">City</label>
+            <input v-on:input="onInput" v-bind:class="validClass" v-bind:value="calcValue" type="text" class="form-control">
+            <div class="invalid-feedback">
+              Please provide a valid city.
+            </div>
+          </div>`
     })
 
     .component("PhoneBookContact", {
@@ -60,29 +178,23 @@ Vue.createApp({})
             }
         },
 
-        // Конструктор компонента - начальное состояние.
-        data() {
-            return {
-                editingText: this.contact.firstName
-            };
-        },
-
         methods: {
             edit() {
-                this.$emit("edit-item", this.editingText);
+                this.$emit("edit-item", [this.contact.id, this.contact.firstName, this.contact.lastName]);
             },
 
-            delete() {
-
+            del() {
+                this.$emit("del-item", this.contact.id);
             }
         },
 
         template: `
           <tr>
             <td>{{ contact.firstName }}</td>
+            <td>{{ contact.lastName }}</td>
             <td>
                 <button class="btn btn-primary" type="button" @click="edit">Edit</button>
-                <button class="btn btn-danger" type="button">Delete</button>
+                <button class="btn btn-danger" type="button" @click="del">Delete</button>
             </td>
           </tr>`
     })
