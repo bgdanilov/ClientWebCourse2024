b1e525258998eb296f52f9e98b1213194bfcea6a
bgdanilov <bdan@yandex.ru>
2024-09-27 21:23:49 +0700

    dfjhgfdljkg

--- PhoneBookBootstrapVue/scripts/phonebook_vue_bootstrap.js
+++ PhoneBookBootstrapVue/scripts/phonebook_vue_bootstrap.js
@@ -12,7 +12,6 @@ Vue.createApp({})
                 editingContact: {},
                 editingContactId: "",
 
-                isContactFormReset: true,
                 isFirstNameFieldReset: true,
                 isLastNameFieldReset: true,
 
@@ -23,6 +22,14 @@ Vue.createApp({})
 
         methods: {
             onSubmit(event) {
+                if (this.isFirstNameFieldValid) {
+                    this.isFirstNameFieldReset = false;
+                }
+
+                if (this.isLastNameFieldValid) {
+                    this.isLastNameFieldReset = false;
+                }
+
                 const submitButtonPressed = event.submitter.name;
 
                 if (submitButtonPressed === "add") {
@@ -34,7 +41,6 @@ Vue.createApp({})
 
             onEdit(event) {
                 this.editingContactId = event[0];
-
                 this.firstNameFieldText = event[1];
                 this.lastNameFieldText = event[2];
 
@@ -46,22 +52,26 @@ Vue.createApp({})
             },
 
             addContact() {
-                console.log(this.isFirstNameFieldValid);
-                console.log(this.isLastNameFieldValid);
-
-                if (this.isFirstNameFieldValid && this.isLastNameFieldValid) {
-                    const newContact = {
-                        id: ++this.initialContactId,
-                        firstName: this.firstNameFieldText,
-                        lastName: this.lastNameFieldText
-                    };
-
-                    this.contacts.push(newContact);
-                    this.resetContactForm();
-                } else {
-                    this.isFirstNameFieldReset = false;
-                    this.isLastNameFieldReset = false;
+                if (!this.isFirstNameFieldValid || !this.isLastNameFieldValid) {
+                    //this.isFirstNameFieldValid = false;
+                    //this.isLastNameFieldValid = false;
+                    return;
                 }
+
+                const newContact = {
+                    id: ++this.initialContactId,
+                    firstName: this.firstNameFieldText,
+                    lastName: this.lastNameFieldText
+                };
+
+                this.contacts.push(newContact);
+
+                this.isFirstNameFieldReset = false;
+                this.isLastNameFieldReset = false;
+                this.firstNameFieldText = "";
+                this.lastNameFieldText = "";
+                this.isFirstNameFieldValid = false;
+                this.isLastNameFieldValid = false;
             },
 
             saveContact() {
@@ -73,40 +83,22 @@ Vue.createApp({})
 
                 this.editingContact.firstName = this.firstNameFieldText;
                 this.editingContact.lastName = this.lastNameFieldText;
-
-                this.resetContactForm();
             },
 
             deleteContact() {
                 const deletedContactIndex = this.contacts.findIndex(contact => contact.id === this.editingContactId);
                 this.contacts.splice(deletedContactIndex, 1);
-                this.resetContactForm();
-            },
-
-            resetContactForm() {
-                this.firstNameFieldText = "";
-                this.lastNameFieldText = "";
-
-                this.isFirstNameFieldReset = true;
-                this.isLastNameFieldReset = true;
-
-                this.isFirstNameFieldValid = false;
-                this.isLastNameFieldValid=  false;
-
-                this.isEditing = false;
-            },
+            }
         },
 
         template:  `
           <form @submit.prevent="onSubmit" class="row needs-validation" novalidate>
-            <text-input :text=this.firstNameFieldText
-                        :isReset="isFirstNameFieldReset"
-                        :isValid="isFirstNameFieldValid"
-                        @update="firstNameFieldText = $event[0]; isFirstNameFieldValid = $event[1]; isFirstNameFieldReset=false"></text-input>
-            <text-input :text=this.lastNameFieldText
-                        :isReset="isLastNameFieldReset"
-                        :isValid="isLastNameFieldValid"
-                        @update="lastNameFieldText = $event[0]; isLastNameFieldValid = $event[1]; isLastNameFieldReset=false"></text-input>
+            <text-input :fieldText="firstNameFieldText"
+                        :isFieldReset="isFirstNameFieldReset"
+                        @update="firstNameFieldText = $event[0]; isFirstNameFieldValid = $event[1];"></text-input>
+            <text-input :fieldText="lastNameFieldText"
+                        :isFieldReset="isLastNameFieldReset"
+                        @update="lastNameFieldText = $event[0]; isLastNameFieldValid = $event[1];"></text-input>
             <div class="col">
                 <div v-if="!isEditing" class="col-auto">
                   <button name="add" class="btn btn-primary">Add</button>
@@ -128,43 +120,77 @@ Vue.createApp({})
     })
 
     .component("TextInput", {
-        props: ['isReset', 'text'],
+        props: ['fieldText', 'isFieldReset'],
 
         // Конструктор компонента - начальное состояние.
         data() {
             return {
-                isFieldValid: false,
+                inputValue: "",
+                fieldTextMessage: "",
+                //isFieldReset1: true,
             };
+        },
+
+        watch: {
+          isFieldReset() {
+              if (this.isFieldReset) {
+                  this.isFieldReset1 = true;
+              } else {
+                  this.isFieldReset1 = false;
+              }
+
+              console.log(this.isFieldReset1);
+             //this.isFieldReset1 = true;
+            }
+        },
+
+        // Это поля как в data, только они вычисляемые! Зависимые от чего-то.
+        computed: {
+           calcValue() {
+               console.log(this.isFieldReset1);
+               return this.fieldText;
+            },
+
+            isFieldReset1() {
+               console.log(this.isFieldReset);
+
+                if (this.isFieldReset) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+
+            isFieldValid() {
+               if (this.inputValue.length === 0) {
+                   this.fieldTextMessage = "Введите значение!";
+                   return false;
+               } else {
+                   return true;
+                }
+            },
         },
 
         methods: {
             onInput(e) {
-                this.$emit('update', [e.target.value, this.isFieldValid]);
-            }
-        },
-
-        computed: {
-            calcValue() {
-                this.isFieldValid = this.text.trim().length !== 0;
-                console.log("isFieldValid = " + this.isFieldValid);
-                return this.text;
+                //this.isFieldReset1 = false; // при вводе поле уже не сброшенное.
+                this.inputValue = e.target.value;
+                this.$emit('update', [this.inputValue, this.isFieldValid]);
             },
-
-            validClass() {
-                if (this.isReset) {
-                    return  "";
-                } else {
-                    return  this.isFieldValid ? "is-valid" : "is-invalid";
-                }
-            }
         },
 
         template: `
           <div class="col">
             <label for="validationCustom03" class="form-label">City</label>
-            <input v-on:input="onInput" v-bind:class="validClass" v-bind:value="calcValue" type="text" class="form-control">
+            <input type="text"
+                   class="form-control"
+                   v-on:input="onInput"
+                   v-bind:value="calcValue"
+                   :class="{'is-invalid': !isFieldValid && !isFieldReset1,
+                            'is-valid': isFieldValid && !isFieldReset1,
+                            '': isFieldReset1}">
             <div class="invalid-feedback">
-              Please provide a valid city.
+              {{ fieldTextMessage }}
             </div>
           </div>`
     })
